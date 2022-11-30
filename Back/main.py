78471737d9ce3197be7c0b7e5b1d4debddb711f8
David <dnhijuelos@gmail.com>
2022-11-30 10:38:30 +0200

    third

--- Back/main.py
+++ Back/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from model import MilaSchema
+from database import fetch_one_mila, fetch_all_milim, new_mila, update_mila, delete_mila
 
 app = FastAPI()
 
@@ -7,32 +9,51 @@ origins = ['https://localhost:3000']
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = origins,
-    allow_credentials = True, 
-    allow_methods = ['*'],
-    allow_headers = ['*'],
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
+
 
 @app.get("/")
 def read_root():
-    return {"ping":"pong"}
+    return {"Spanish": "Hebrew"}
+
 
 @app.get("/api/mila")
 async def get_mila():
-    return 1
+    response = await fetch_all_milim()
+    return response
 
-@app.get("/api/mila{id}")
-async def get_mila_id(id):
-    return 1
 
-@app.post("api/mila")
-async def post_mila(mila):
-    return 1 
+@app.get("/api/mila{spanish}", response_model=MilaSchema)
+async def get_one_mila(spanish):
+    response = await fetch_one_mila(spanish)
+    if response:
+        return response
+    raise HTTPException(404, f"the word {spanish} wasn't found")
 
-@app.put("api/mila{id}")
-async def update_mila(id, data):
-    return 1 
 
-@app.delete("api/milon{id}")
-async def delete_mila(id):
-    return 1 
+@app.post("/api/mila", response_model=MilaSchema)
+async def post_mila(mila: MilaSchema):
+    response = await new_mila(mila.dict())
+    if response:
+        return response
+    raise HTTPException(400, "something went wrong")
+
+
+@app.put("/api/mila{spanish}", response_model=MilaSchema)
+async def put_mila(spanish: str, hebrew: str, fonetica: str):
+    response = await update_mila(spanish, hebrew, fonetica)
+    if response:
+        return response
+    raise HTTPException(400, "something went wrong")
+
+
+@app.delete("/api/milon{spanish}")
+async def remove_mila(spanish):
+    response = await delete_mila(spanish)
+    if response:
+        return "Succesfully deleted"
+    raise HTTPException(404, f"the word {spanish} wasn't found")
--- Back/model.py
+++ Back/model.py
@@ -1,6 +1,44 @@
-from pydantic import BaseModel
+from typing import Optional
+from pydantic import BaseModel, Field
 
-class Mila(BaseModel):
-    spanish: str
-    hebrew: str
-    fonetica: str
+
+class MilaSchema(BaseModel):
+    spanish: str = Field(...)
+    hebrew: str = Field(...)
+    fonetica: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "spanish": "hola",
+                "hebrew": "שלום",
+                "fonetica": "shalom",
+            }
+        }
+
+
+class UpdateMilaModel(BaseModel):
+    spanish: Optional[str]
+    hebrew: Optional[str]
+    fonetica: Optional[str]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "spanish": "hola",
+                "hebrew": "שלום",
+                "fonetica": "shalom",
+            }
+        }
+
+
+def ResponseModel(data, message):
+    return {
+        "data": [data],
+        "code": 200,
+        "message": message,
+    }
+
+
+def ErrorResponseModel(error, code, message):
+    return {"error": error, "code": code, "message": message}
